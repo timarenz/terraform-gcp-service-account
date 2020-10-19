bb680bd53701a6c9e0e0727142d402dc770f48ac
Salim Afiune Maya <afiune@lacework.net>
2020-10-20 00:31:35 +0200

    💯 initial commit and v0.1.0 release
    Signed-off-by: Salim Afiune Maya <afiune@lacework.net>

---
+++ README.md
@@ -0,0 +1,8 @@
+<a href="https://lacework.com"><img src="https://techally-content.s3-us-west-1.amazonaws.com/public-content/lacework_logo_full.png" width="600"></a>
+
+# terraform-gcp-sercice-account
+
+[![GitHub release](https://img.shields.io/github/release/lacework/terraform-gcp-sercice-account.svg)](https://github.com/lacework/terraform-gcp-sercice-account/releases/)
+[![CircleCI status](https://circleci.com/gh/lacework/terraform-gcp-sercice-account.svg?style=shield)](https://circleci.com/gh/lacework/terraform-gcp-sercice-account)
+
+Terraform module that creates a service account to provide Lacework read-only access to Google Cloud Platform Organizations and Projects.
---
+++ examples/custom-organization-level-service-account/main.tf
@@ -0,0 +1,20 @@
+// This template assumes the default configuration coming from the following
+// environment variables 'GOOGLE_CREDENTIALS' and 'GOOGLE_PROJECT'
+//
+// Example of how to run this code:
+//
+// $ terraform init
+// $ GOOGLE_CREDENTIALS=account.json GOOGLE_PROJECT=my-project terraform apply
+//
+// Additionally, you could add the credentials and project configuration inside
+// the google provider, to do so, look at the following instructions:
+// => https://registry.terraform.io/providers/hashicorp/google/latest/docs
+provider "google" {}
+
+module "lacework_svc_account" {
+  source               = "../../"
+  org_integration      = true
+  organization_id      = "my-organization-id"
+  project_id           = "a-different-project-id"
+  service_account_name = "lacework-custom-svc-account"
+}
